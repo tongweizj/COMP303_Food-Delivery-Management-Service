7b833fd46b3dc0e3218dc880c526513b51a9bf7f
Max <tongweizj@gmail.com>
2026-04-11 20:57:05 -0400

    Initialize Microservice gateway #3

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8084/api',
+  baseURL: "http://localhost:8080/api",
 });
 
 export default apiClient;
